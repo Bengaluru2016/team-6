778576d88839477c1f14a29668bf7c94c1dc6ad0
Rishabh Saxena <rishabh.saxena1994@gmail.com>
2016-07-10 03:37:22 +0530

    email change1

--- email/email.php
+++ email/email.php
@@ -1,17 +1,12 @@
+
 <?php
-# Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
+$to      = 'koushal.ogirala@gmail.com';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: derangtest@gmail.com' . "\r\n" .
+    'Reply-To: derangtest@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-# Instantiate the client.
-$mgClient = new Mailgun('key-a7ea7ab1bbcb6f813232fdd2e60fdd66');
-$domain = "sandbox2f2bded26dd643e28682deec4a3fb058.mailgun.org";
-
-# Make the call to the client.
-$result = $mgClient->sendMessage($domain, array(
-    'from'    => 'Excited User <postmaster@sandbox2f2bded26dd643e28682deec4a3fb058.mailgun.org>',
-    'to'      => 'Baz <koushal.ogirala@gmail.com>',
-    'subject' => 'Hello',
-    'text'    => 'Testing some Mailgun awesomness!'
-));
+mail($to, $subject, $message, $headers);
 ?>
+
