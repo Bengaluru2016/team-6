704047b690f645d3f0476fdb42903081f6c5553f
Rishabh Saxena <rishabh.saxena1994@gmail.com>
2016-07-10 02:48:39 +0530

    email change

--- email/email.php
+++ email/email.php
@@ -5,12 +5,12 @@ use Mailgun\Mailgun;
 
 # Instantiate the client.
 $mgClient = new Mailgun('key-a7ea7ab1bbcb6f813232fdd2e60fdd66');
-$domain = "rishabh11.mydomain.com";
+$domain = "sandbox2f2bded26dd643e28682deec4a3fb058.mailgun.org";
 
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
-    'from'    => 'Excited User <mailgun@rishabh11.mydomain.com>',
-    'to'      => 'Baz <commonbazinga@gmail.com>',
+    'from'    => 'Excited User <postmaster@sandbox2f2bded26dd643e28682deec4a3fb058.mailgun.org>',
+    'to'      => 'Baz <koushal.ogirala@gmail.com>',
     'subject' => 'Hello',
     'text'    => 'Testing some Mailgun awesomness!'
 ));
