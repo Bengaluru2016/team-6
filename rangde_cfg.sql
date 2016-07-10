6562cb46ccd0659c755124af12497e0f6d354db5
koushal <koushal.ogirala@gmail.com>
2016-07-10 07:10:20 +0530

    added extra columns

--- rangde_cfg.sql
+++ rangde_cfg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2016 at 03:16 PM
+-- Generation Time: Jul 10, 2016 at 03:39 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.15
 
@@ -33,17 +33,31 @@ CREATE TABLE `user` (
   `mobile` bigint(10) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `lastname` varchar(20) NOT NULL,
-  `rank` varchar(20) NOT NULL
+  `rank` int(10) NOT NULL,
+  `points` int(10) NOT NULL,
+  `profile` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `email`, `mobile`, `firstname`, `lastname`, `rank`) VALUES
-('koushal', '123456', 'koushal.ogirala@gmail.com', 9553748667, 'Prudhvi', 'Koushal', ''),
-('ranjit95', '123456', 'a@a.c', 9999999999, 'surya', 'ranjit', ''),
-('vsd', '12345', 'a@a.c', 9874561230, 'k', 'l', '');
+INSERT INTO `user` (`username`, `password`, `email`, `mobile`, `firstname`, `lastname`, `rank`, `points`, `profile`) VALUES
+('afsfdf', '789456', 'a@e.f', 4445556667, 'kk', 'lap', 0, 0, ''),
+('afsfdfadadaa', '147258', 'a@e.faa', 4445556666, 'kkfg', 'lapaa', 0, 0, ''),
+('koushal', '123456', 'koushal.ogirala@gmail.com', 9553748666, 'Prudhvi', 'Koushal', 0, 1000, 'https://github.com/koushal95'),
+('az', '123456', 'q@w.e', 1478529632, 'abcd', 'wxyz', 0, 0, ''),
+('ranjit', '123456', 'tss@ab.c', 1234567891, 'surya', 'ranjit', 0, 800, 'https://github.com/ranjit95');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
